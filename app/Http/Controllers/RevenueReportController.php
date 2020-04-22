e5759b52c234a9e04e922f9d96231f576c6870bc
Pressiix <watcharapon.piam@gmail.com>
2020-04-22 18:56:11 +0700

    ...

--- app/Http/Controllers/RevenueReportController.php
+++ app/Http/Controllers/RevenueReportController.php
@@ -23,6 +23,63 @@ use Input;
 
 class RevenueReportController extends Controller
 {
+    public function test()
+    {
+        $item = [];
+        $bp = [];
+        $ptd=[];
+        $bp_index=0;
+        $ptd_index=0;
+        $request = RequestForm::all();
+        $addesc = AdDescription::all();
+        for($i=0;$i<count($request);$i++)
+        {
+            $item1[$request[$i]->getOriginal()['id']] = $request[$i]->getOriginal();
+        }
+        for($j=0;$j<count($addesc);$j++)
+        {
+            $item2[$addesc[$j]->getOriginal()['request_id']] = $addesc[$j]->getOriginal();
+            unset($item2[$addesc[$j]->getOriginal()['request_id']]['id'] ,$item2[$addesc[$j]->getOriginal()['request_id']]['request_id']);
+        }
+        $item = array_values(array_replace_recursive($item1,$item2));
+        for($k=0;$k<count($item);$k++)
+        {
+            if($item[$k]['status'] == 'Waiting')
+            {
+                unset($item[$k]);
+            }
+        }
+        $item = array_values($item);
+        
+        foreach($item as $key=>$value)
+        {
+            $field = array_keys($value);
+            foreach($field as $key2)
+            {
+                if(is_array(json_decode($item[$key][$key2],true)))
+                {
+                    //echo $key2."<br/>";
+                    if(strpos($key2,"bp_") == true)
+                    {
+                        $bp[$bp_index][$key2] = json_decode($item[$key][$key2],true);
+                    }
+                    if(strpos($key2,"ptd_") == true)
+                    {
+                        $ptd[$ptd_index][$key2] = json_decode($item[$key][$key2],true);
+                    }
+                    
+                }else{
+                    if(strpos($key2,"bp_") == false)
+                        $bp[$bp_index][$key2] = $item[$key][$key2];
+                        $ptd[$ptd_index][$key2] = $item[$key][$key2];
+                }
+            }
+            $bp_index++;
+            $ptd_index++;
+        } 
+        echo "<pre/>"; print_r($bp);
+    }
+
     public function index()
     {
         return view('new.revenue');
