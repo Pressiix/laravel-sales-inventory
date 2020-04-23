ef5c2a151554425ba389c1a9e1d0f2c2db09d31b
Pressiix <watcharapon.piam@gmail.com>
2020-04-24 00:18:53 +0700

    create function for generate revenue report detail

--- app/Http/Controllers/RevenueReportController.php
+++ app/Http/Controllers/RevenueReportController.php
@@ -23,11 +23,6 @@ use Input;
 
 class RevenueReportController extends Controller
 {
-    private $field = array(
-        "update_at",
-        "sales_name",
-        "campaign_name",
-    );
 
     public function test()
     {
@@ -48,12 +43,14 @@ class RevenueReportController extends Controller
             unset($item2[$addesc[$j]->getOriginal()['request_id']]['id'] ,$item2[$addesc[$j]->getOriginal()['request_id']]['request_id']);
         }
         $item = array_values(array_replace_recursive($item1,$item2));
-        for($k=0;$k<count($item);$k++)
+        $count = count($item);
+        for($k=0;$k<$count;$k++)
         {
             if($item[$k]['status'] == 'Waiting')
             {
                 unset($item[$k]);
             }
+
         }
         $item = array_values($item);
         
@@ -64,17 +61,42 @@ class RevenueReportController extends Controller
             {
                 if(is_array(json_decode($item[$key][$key2],true)))
                 {
-                    //$item[$key][$key2] = array_values($item[$key][$key2]);
-                    //echo $key2."<br/>";
-                    if(strpos($key2,"bp_") !== false)
-                    {
-                        $bp[$bp_index][$key2] = array_values(json_decode($item[$key][$key2],true));
-                    }
-                    if(strpos($key2,"ptd_") !== false)
-                    {
-                        $ptd[$ptd_index][$key2] = array_values(json_decode($item[$key][$key2],true));
-                    }
-                    
+                    if($key2 !== "bp_web" && $key2 !== "ptd_web"){
+                        if(strpos($key2,"bp_") !== false)
+                        {
+                            if(count(json_decode($item[$key][$key2],true)) > 1)
+                            {
+                                $bp[$bp_index][$key2] = array_values(json_decode($item[$key][$key2],true));
+                            }
+                            else if(count(json_decode($item[$key][$key2],true)) == 1){
+                                if(isset(json_decode($item[$key][$key2],true)[array_key_first(json_decode($item[$key][$key2],true))]))
+                                {
+                                    $bp[$bp_index][$key2] = json_decode($item[$key][$key2],true)[array_key_first(json_decode($item[$key][$key2],true))];
+
+                                }else{
+                                    unset($bp[$bp_index]);
+                                    break;
+                                }
+                            }
+                        }
+                        if(strpos($key2,"ptd_") !== false)
+                        {
+                            if(count(json_decode($item[$key][$key2],true)) > 1)
+                            {
+                                $ptd[$ptd_index][$key2] = array_values(json_decode($item[$key][$key2],true));
+                            }
+                            else if(count(json_decode($item[$key][$key2],true)) == 1 ){
+                                if(isset(json_decode($item[$key][$key2],true)[array_key_first(json_decode($item[$key][$key2],true))]))
+                                {
+                                    $ptd[$ptd_index][$key2] = json_decode($item[$key][$key2],true)[array_key_first(json_decode($item[$key][$key2],true))];
+
+                                }else{
+                                    unset($ptd[$ptd_index]);
+                                    break;
+                                }
+                            }
+                        }
+                    } 
                 }else{
                     if(strpos($key2,"bp_") !== false && strpos($key2,"ptd_") == false){
                         $bp[$bp_index][$key2] = $item[$key][$key2];
@@ -92,9 +114,53 @@ class RevenueReportController extends Controller
             $bp_index++;
             $ptd_index++;
         }
-        $revenue_detail['bangkokpost'] = $bp;
-        $revenue_detail['posttoday'] = $ptd;
-        echo "<pre/>"; print_r($revenue_detail);
+        $bp = array_values($bp);
+        $ptd = array_values($ptd);
+        
+        $revenue_detail['bangkokpost'] = $this->getReportDetail($bp,"bp");
+        $revenue_detail['posttoday'] = $this->getReportDetail($ptd,"ptd");
+       echo "<pre/>"; print_r($revenue_detail);
+    }
+
+    private function getReportDetail($web,$web_name)
+    {
+        $x=0;
+        $detail=[];
+        foreach($web as $web_index=>$web_item)
+        {
+                foreach($web_item as $key=>$value)
+                {
+                    if(is_array($value))
+                    {
+                        $detail[$x][$key] = $value[0];
+                    }
+                    else
+                    {
+                        $detail[$x][$key] = $value;
+                        if(strpos($key,$web_name."_campaign_budget")!== false && is_array($web[$web_index][$web_name.'_size'])){
+                            foreach($web[$web_index][$web_name.'_size'] as $key2=>$value2)
+                            {
+                                if($key2 !== 0)
+                                {
+                                    $x++;
+                                    foreach(array_keys($web[$web_index]) as $web_key)
+                                    {
+                                        if(is_array($web[$web_index][$web_key]))
+                                        {
+                                            $detail[$x][$web_key] = $web[$web_index][$web_key][$key2];
+                                        }
+                                        else{
+                                            $detail[$x][$web_key] = $web[$web_index][$web_key];
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                $x++;
+        }
+        return $detail;
     }
 
     public function index()
