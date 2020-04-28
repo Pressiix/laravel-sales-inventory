d885b2e45c1737b1354c7ef448c8484f64bcda16
Pressiix <watcharapon.piam@gmail.com>
2020-04-28 12:04:55 +0700

    remove unued files

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'watcharapon.piam@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'bpinventory.test@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'System'),
     ],
 
