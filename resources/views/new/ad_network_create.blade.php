9628b6a494d8996016559c9d0e0aa66c21239b36
Watcharaphon Piamphuetna <>
2020-05-12 17:20:07 +0700

    AdNetwork : add script for calculate ecpm value

--- resources/views/new/ad_network_create.blade.php
+++ resources/views/new/ad_network_create.blade.php
@@ -14,7 +14,7 @@
                 <label for="inputCampaign" class="col-sm-4 col-md-4 col-lg-3 col-form-label">Start Date:</label>
                   <div class="col-sm-4">
                     <div class="input-group-inline">
-                      <input type="text" class="form-control form-input--date d2" name="start" value="<?= (isset($item['start']) ? $item['start'] : '' ) ?>" autocomplete="off">
+                      <input type="text" class="form-control form-input--date d2" name="start" value="<?= (isset($item['start']) ? $item['start'] : '' ) ?>" autocomplete="off" required>
                       <span><img src="assets/images/icon-svg/calendar.svg" width="20"></span>
                     </div>
                   </div>
@@ -23,7 +23,7 @@
                   </div>
                   <div class="col-sm-4">
                     <div class="input-group-inline">
-                      <input type="text" class="form-control form-input--date d2" name="end" value="<?= (isset($item['start']) ? $item['end'] : '' ) ?>" autocomplete="off">
+                      <input type="text" class="form-control form-input--date d2" name="end" value="<?= (isset($item['start']) ? $item['end'] : '' ) ?>" autocomplete="off" required>
                       <span><img src="assets/images/icon-svg/calendar.svg" width="20"></span>
                     </div>
                   </div>
@@ -45,13 +45,13 @@
                       <div class="form-group row">
                         <label for="customerSelect" class="col-sm-4 col-md-4 col-lg-3 col-form-label">Advertiser:</label>
                         <div class="col-sm-11 col-md-11 col-lg-12">
-                        <select class="custom-select" name="advertiser_id[0]" onchange="document.getElementById('advertiser_name').value=this.options[this.selectedIndex].text">
+                        <select class="custom-select" name="advertiser_id[0]" onchange="document.getElementById('advertiser_name').value=this.options[this.selectedIndex].text" required>
                           <option value="">Choose...</option>
                           <?php foreach($advertiser as $key=>$value){ ?>
                             <option value="<?= $key ?>"><?= $value ?></option>
                           <?php } ?>
                         </select>
-                        <input type="hidden" name="advertiser_name[0]" id="advertiser_name" value="<?= (isset($item['advertiser_name']) ? $item['advertiser_name'] : '') ?>" />
+                        <input type="hidden" name="advertiser_name[0]" id="advertiser_name" value="<?= (isset($item['advertiser_name']) ? $item['advertiser_name'] : '') ?>"  required>
                           <div class="div-form--link"> or <a href="javascript:;" id="btn-addnew0" onclick="$('#addnews-advertiser'+this.id.substring(10,this.id.length,10)).show();">Add new</a></div>
                         </div>
                       </div>
@@ -61,7 +61,7 @@
                             <div class="form-group row">
                               <label for="inputCampaign" class="col-sm-4 col-md-4 col-lg-3 col-form-label">New Advertiser</label>
                               <div class="col-sm-11 col-md-11 col-lg-12">
-                                <input type="text" class="form-control" name="new_advertiser[0]">
+                                <input type="text" class="form-control" name="new_advertiser[0]" required>
                               </div>
                             </div>
                           </div>
@@ -70,25 +70,25 @@
                       <div class="form-group row">
                         <label for="inputCampaign" class="col-sm-4 col-md-4 col-lg-3 col-form-label">Pageview:</label>
                         <div class="col-sm-11 col-md-11 col-lg-12">
-                          <input type="text" class="form-control" name="pageview[0]">
+                          <input type="text" class="form-control" name="pageview[0]" required>
                         </div>
                       </div>
                       <div class="form-group row">
                         <label for="inputCampaign" class="col-sm-4 col-md-4 col-lg-3 col-form-label">Impression:</label>
                         <div class="col-sm-11 col-md-11 col-lg-12">
-                          <input type="text" class="form-control" name="impression[0]">
+                          <input type="text" class="form-control" id="impression0" name="impression[0]" required>
                         </div>
                       </div>
                       <div class="form-group row">
                         <label for="inputCampaign" class="col-sm-4 col-md-4 col-lg-3 col-form-label">eCPM:</label>
                         <div class="col-sm-11 col-md-11 col-lg-12">
-                          <input type="text" class="form-control" name="ecpm[0]">
+                          <input type="text" class="form-control" placeholder="Impression and Revenue field has been required" id="ecpm0" name="ecpm[0]" disabled required>
                         </div>
                       </div>
                       <div class="form-group row">
                         <label for="inputCampaign" class="col-sm-4 col-md-4 col-lg-3 col-form-label">Revenue (THB):</label>
                         <div class="col-sm-11 col-md-11 col-lg-12">
-                          <input type="text" class="form-control" name="revenue[0]">
+                          <input type="text" class="form-control" id="revenue0" name="revenue[0]" required>
                         </div>
                       </div>
                     </div>
@@ -102,13 +102,13 @@
                       <div class="form-group row">
                         <label for="customerSelect" class="col-sm-4 col-md-4 col-lg-3 col-form-label">Advertiser:</label>
                         <div class="col-sm-11 col-md-11 col-lg-12">
-                        <select class="custom-select" name="advertiser_id[<?= $i ?>]" onchange="document.getElementById('advertiser_name').value=this.options[this.selectedIndex].text">
+                        <select class="custom-select" name="advertiser_id[<?= $i ?>]" onchange="document.getElementById('advertiser_name').value=this.options[this.selectedIndex].text"  required>
                           <option <?= (!isset($item['advertiser_id'][$i]) ? 'selected' : '') ?> >Choose...</option>
                           <?php foreach($advertiser as $key=>$value){ ?>
                             <option <?= (isset($item['advertiser_id'][$i]) && $item['advertiser_id'][$i] ==$key ? 'selected' : '') ?> value="<?= $key ?>"><?= $value ?></option>
                           <?php } ?>
                         </select>
-                        <input type="hidden" name="advertiser_name[<?= $i ?>]" id="advertiser_name" value="<?= (isset($item['advertiser_name'][$i]) ? $item['advertiser_name'][$i] : '') ?>" />
+                        <input type="hidden" name="advertiser_name[<?= $i ?>]" id="advertiser_name" value="<?= (isset($item['advertiser_name'][$i]) ? $item['advertiser_name'][$i] : '') ?>"  required>
                           <div class="div-form--link"> or <a href="javascript:;" id="btn-addnew1" onclick="$('#addnews-advertiser<?= $i ?>').show();">Add new</a></div>
                         </div>
                       </div>
@@ -118,7 +118,7 @@
                             <div class="form-group row">
                               <label for="inputCampaign" class="col-sm-4 col-md-4 col-lg-3 col-form-label">New Advertiser</label>
                               <div class="col-sm-11 col-md-11 col-lg-12">
-                                <input type="text" class="form-control" name="new_advertiser[<?= $i ?>]" value="<?= (isset($item['new_advertiser'][$i]) ? $item['new_advertiser'][$i] : '') ?>" >
+                                <input type="text" class="form-control" name="new_advertiser[<?= $i ?>]" value="<?= (isset($item['new_advertiser'][$i]) ? $item['new_advertiser'][$i] : '') ?>"  required>
                               </div>
                             </div>
                           </div>
@@ -127,25 +127,25 @@
                       <div class="form-group row">
                         <label for="inputCampaign" class="col-sm-4 col-md-4 col-lg-3 col-form-label">Pageview:</label>
                         <div class="col-sm-11 col-md-11 col-lg-12">
-                          <input type="text" class="form-control" name="pageview[<?= $i ?>]" value="<?= (isset($item['pageview'][$i]) ? $item['pageview'][$i] : '') ?>" >
+                          <input type="text" class="form-control" name="pageview[<?= $i ?>]" value="<?= (isset($item['pageview'][$i]) ? $item['pageview'][$i] : '') ?>"  required>
                         </div>
                       </div>
                       <div class="form-group row">
                         <label for="inputCampaign" class="col-sm-4 col-md-4 col-lg-3 col-form-label">Impression:</label>
                         <div class="col-sm-11 col-md-11 col-lg-12">
-                          <input type="text" class="form-control" name="impression[<?= $i ?>]" value="<?= (isset($item['impression'][$i]) ? $item['impression'][$i] : '') ?>" >
+                          <input type="text" class="form-control" id="impression<?= $i ?>" name="impression[<?= $i ?>]" value="<?= (isset($item['impression'][$i]) ? $item['impression'][$i] : '') ?>"  required>
                         </div>
                       </div>
                       <div class="form-group row">
                         <label for="inputCampaign" class="col-sm-4 col-md-4 col-lg-3 col-form-label">eCPM:</label>
                         <div class="col-sm-11 col-md-11 col-lg-12">
-                          <input type="text" class="form-control" name="ecpm[<?= $i ?>]" value="<?= (isset($item['ecpm'][$i]) ? $item['ecpm'][$i] : '') ?>" >
+                          <input type="text" class="form-control" placeholder="Impression and Revenue field has been required" id="ecpm<?= $i ?>" name="ecpm[<?= $i ?>]" value="<?= (isset($item['ecpm'][$i]) ? $item['ecpm'][$i] : '') ?>"  required>
                         </div>
                       </div>
                       <div class="form-group row">
                         <label for="inputCampaign" class="col-sm-4 col-md-4 col-lg-3 col-form-label">Revenue (THB):</label>
                         <div class="col-sm-11 col-md-11 col-lg-12">
-                          <input type="text" class="form-control" name="revenue[<?= $i ?>]" value="<?= (isset($item['revenue'][$i]) ? $item['revenue'][$i] : '') ?>" >
+                          <input type="text" class="form-control" id="revenue<?= $i ?>" name="revenue[<?= $i ?>]" value="<?= (isset($item['revenue'][$i]) ? $item['revenue'][$i] : '') ?>"  required>
                         </div>
                       </div>
                     </div>
@@ -173,9 +173,46 @@
             orientation: "bottom"
         });
 
+        
+          $(document).on("keyup", 'input[id*="revenue"]', function () {
+            var id = this.id;
+            var index = id[id.length -1];
+            var impression = parseFloat($('input[id="impression'+index+'"]').val());
+            var revenue = parseFloat(this.value);
+            if($('input[id="impression'+index+'"]').val().length !== 0 && this.value.length !== 0)
+            {
+              if(!isNaN((revenue/impression)*1000))
+              {
+                $('input[id="ecpm'+index+'"]').val(((revenue/impression)*1000).toFixed(2));
+              }else{
+                $('input[id="ecpm'+index+'"]').val("");
+              }
+            }else{
+              $('input[id="ecpm'+index+'"]').val("");
+            }
+          });
+
+          $(document).on("keyup", 'input[id*="impression"]', function () {
+            var id = this.id;
+            var index = id[id.length -1];
+            var revenue = parseFloat($('input[id="revenue'+index+'"]').val());
+            var  impression = parseFloat(this.value);
+            if($('input[id="impression'+index+'"]').val().length !== 0 && this.value.length !== 0)
+            {
+              if(!isNaN((revenue/impression)*1000))
+              {
+                $('input[id="ecpm'+index+'"]').val(((revenue/impression)*1000).toFixed(2));
+              }else{
+                $('input[id="ecpm'+index+'"]').val("");
+              }
+            }else{
+              $('input[id="ecpm'+index+'"]').val("");
+            }
+          });
+
         function addItems(){
             var count = $("div[id*='item-card']").length;
-            console.log(count);
+            //console.log(count);
             var Html= $("div[id*='item-card']").eq(0).clone();
 
                 Html.find("a[id*='btn-addnew']").each(function() {
@@ -190,6 +227,7 @@
 
                 Html.find('input').each(function() {  //Replace input name
                     this.name= this.name.replace('[0]', '['+count+']');
+                    this.id= this.id.replace('0', count);
                 });
                 Html.find('select').each(function() {  //Replace input name
                     this.name= this.name.replace('[0]', '['+count+']');
