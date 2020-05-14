cf477f25f2e45ae3da56aedd6dc66bc691f79fc4
Watcharaphon Piamphuetna <>
2020-05-14 11:12:31 +0700

    add svg file

--- resources/views/new/request_form.blade.php
+++ resources/views/new/request_form.blade.php
@@ -1176,6 +1176,17 @@
       $('.nav-requestForm').removeClass('tabs--ptd');
     })
 
+    window.onbeforeunload = function (e) {
+        e = e || window.event;
+
+        // For IE and Firefox prior to version 4
+        if (e) {
+            e.returnValue = 'Any string';
+        }
+
+        // For Safari
+        return 'Any string';
+    };
 
 </script>
 @endsection
