6414306d69855f6887fe110ddd4737882df2e617
Pressiix <watcharapon.piam@gmail.com>
2020-05-27 20:57:43 +0700

    RQ : show modal

--- resources/views/new/request_form.blade.php
+++ resources/views/new/request_form.blade.php
@@ -1376,23 +1376,16 @@
             });
         });
 
-        var confirmOnPageExit = function (e) {
-// If we haven't been passed the event get the window.event
-e = e || window.event;
+        window.onbeforeunload = function(e) {
+          alert('Hello, pissed off user!');  
+          $('#confirmModal').modal("show");
+          $('#confirmModal').delay(8000).fadeOut(1000);
+          setTimeout(function(){
+            $('#confirmModal').modal("hide");
+          }, 9000);
+        };
 
-var message = "Are you sure you want to navigate away from this page? All unsaved changes will be lost.";
-
-// For IE6-8 and Firefox prior to version 4
-if (e) 
-{
-    e.returnValue = message;
-}
-
-// For Chrome, Safari, IE8+ and Opera 12+
-return message;
-};
-
-window.onbeforeunload = confirmOnPageExit;
+        
 
       </script>
 @endsection
