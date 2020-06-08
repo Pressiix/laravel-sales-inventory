86495386c75f70772d99466080199adc3ae6deb9
Watcharaphon Piamphuetna <>
2020-06-08 11:39:43 +0700

    delete old request before submit new request

--- app/Http/Controllers/RequestFormController.php
+++ app/Http/Controllers/RequestFormController.php
@@ -373,8 +373,8 @@ class RequestFormController extends Controller
             else if($request->input('action') === 'Submit')
             {
                 //DELETE BEFORE SAVE
-                //AdDescription::where('request_id',RequestForm::where('request_id', $request->request_id)->first()->getOriginal()['id'] )->delete();
-                //RequestForm::where('request_id', $request->request_id)->delete();
+                AdDescription::where('request_id',RequestForm::where('request_id', $request->request_id)->first()->getOriginal()['id'] )->delete();
+                RequestForm::where('request_id', $request->request_id)->delete();
                 
                 $request_id = substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, 9);
                 //Save new request and ad description
